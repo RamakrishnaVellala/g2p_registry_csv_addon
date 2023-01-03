c5da55946a04d6bc1ba8c2fc4c2ce24c43fb885b
RamakrishnaVellala <itsp.kadapa@yahoo.com>
2023-01-03 04:01:08 -0500

    placing import button near create

--- g2p_csv_import/static/src/js/import_button.js
+++ g2p_csv_import/static/src/js/import_button.js
@@ -4,29 +4,25 @@ odoo.define('button_near_create.tree_button', function (require) {
     var ListView = require('web.ListView');
     var viewRegistry = require('web.view_registry');
     var TreeButton = ListController.extend({
-       buttons_template: 'button_near_create.buttons',
-       events: _.extend({}, ListController.prototype.events, {
-           'click .open_wizard_action': '_OpenWizard',
-       }),
-       _OpenWizard: function () {
-           var self = this;
+        buttons_template: 'button_near_create.buttons',
+        events: _.extend({}, ListController.prototype.events, {
+            'click .open_wizard_action': '_OpenWizard',
+        }),
+        _OpenWizard: function () {
+            var self = this;
             this.do_action({
-               type: 'ir.actions.act_window',
-               res_model: 'res.partner',
-               name :'import',
-               view_mode: 'form',
-               view_type: 'form',
-               views: [[false, 'form']],
-               target: 'current',
-               res_id: false,
-               context:{'default_is_registrant':1,'default_is_group':0,'registrant_form':1}
+                type: 'ir.actions.client',
+                tag: 'import',
+                params: { 
+                    'model': 'res.partner', 
+                    'context': "{ 'default_is_registrant': 'True', 'default_is_group': 'False','registrant_form': 'True' }" }
             });
-       }
+        }
     });
     var IndividualListView = ListView.extend({
-       config: _.extend({}, ListView.prototype.config, {
-           Controller: TreeButton,
-       }),
+        config: _.extend({}, ListView.prototype.config, {
+            Controller: TreeButton,
+        }),
     });
     viewRegistry.add('button_in_tree', IndividualListView);
-    });
+});
